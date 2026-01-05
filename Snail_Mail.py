fbb7264a08e66fbe0ee4b8ba2b86e122d6ad25a1
Erik Mändmaa <erikmandmaa@gmail.com>
2026-01-06 00:07:49 +0200

    Add files via upload

---
+++ Snail_Mail.py
@@ -0,0 +1,14 @@
+print("I am 17")
+print("EEEE  RRRR  IIII  K   K")
+print("E     R  R  IIII  K  K")
+print("EEEE  RRRR  IIII  K")
+print("E     R R   IIII  K K")
+print("EEEE  R  R  IIII  K   K") 
+
+date = "24.09.2025"
+current_feeling = "tired"
+accomplishment = "get into university"
+message = "become a developer and earn six figures per year or month"
+emoji = ":smileyface:"
+
+print(date, current_feeling, accomplishment, message, emoji)
